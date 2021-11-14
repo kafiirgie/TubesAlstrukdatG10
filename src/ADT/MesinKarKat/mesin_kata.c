6bdd3006b774cfa5ebb2615437a2146454e966e5
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-11-14 17:50:14 +0700

    mesin kar n kat added
    ADT n Implementation of mesin karakter and mesin kata

---
+++ src/ADT/MesinKarKat/mesin_kata.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "mesin_kata.h"
+
+boolean EndKata;
+Kata CKata;
+
+void IgnoreBlank(){
+    while (CC == BLANK && CC != MARK){
+        ADV();
+    }
+}
+
+
+void SalinKata() {
+    int i = 1;
+    for (i = 1;(CC != BLANK) && (CC != MARK) && (i <= NMax);i++){
+        CKata.TabKata[i] = CC ;
+        ADV() ;
+    }
+    CKata.Length = i - 1;
+}
+
+void STARTKATA() {
+    START();
+    IgnoreBlank();
+    EndKata = (CC==MARK);
+    if (EndKata == TRUE) {
+        ADVKATA();
+    }
+}
+
+void ADVKATA() {
+    IgnoreBlank();
+    if (CC==MARK && !EndKata) {
+        EndKata = TRUE;
+    } else {
+        SalinKata();
+    }
+}
